4efa147faec9c810cf742f5ddadbab5bc0148c9a
fhamyla <fhamyla.devera@gmail.com>
2025-01-19 18:06:55 +0800

    added app icons images

--- Simulator/MainPage.xaml.cs
+++ Simulator/MainPage.xaml.cs
@@ -1,17 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.Maui.Controls;
-using System.Diagnostics;
-using System.Threading.Tasks;
+﻿using Microsoft.Maui.Controls;
 
 namespace Simulator
 {
     public partial class MainPage : ContentPage
     {
-        
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void ChangeWallpaper()
+        {
+            // Let user select an image from their device
+            var result = await FilePicker.PickAsync(new PickOptions
+            {
+                FileTypes = FilePickerFileType.Images,
+                PickerTitle = "Select a Wallpaper"
+            });
+
+            if (result != null)
+            {
+                Wallpaper.Source = ImageSource.FromFile(result.FullPath);
+            }
         }
     }
 }
